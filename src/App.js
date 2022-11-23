4b10e60883c869992ac0ce853bcafdf21973e1d9
soundarya <srikanta.s@northeastern.edu>
2022-11-22 20:28:59 -0500

    initial project setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import NavBar from './navbar-component';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <h1>Web Squad!</h1> */}
+      <NavBar/>
+    </div>
+  );
+}
+
+export default App;
